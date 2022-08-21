d11b3a521bd21eadce99dfb4a39b58a5fbd7ad67
AntonioCorralG <97654161+AntonioCorralG@users.noreply.github.com>
2022-08-20 20:02:03 -0700

    Adding update app.js file with routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,43 +2,52 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Provider from "./components/Context";
 import "./App.css";
-// import Authenticated from './components/Authenticated';
-// import UserSignIn from './components/Users/UserSignIn';
+import Authenticated from './components/Authenticated';
+import UserSignIn from './components/Users/UserSignIn';
 import Header from './/components/Header';
 import UserSignUp from './components/Users/UserSignUp';
-// import UserSignOut from './components/Users/UserSignOut';
-import Public from "./components/Public";
-// import NotFound from './components/NotFound';
+import UserSignOut from './components/Users/UserSignOut';
+// import Public from "./components/Public";
+import NotFound from './components/NotFound';
+import Courses from './components/Courses/Courses'
+import CourseDetail from './components/Courses/CourseDetail';
+import CreateCourse from './components/Courses/CreateCourse';
+import UpdateCourse from './components/Courses/UpdateCourse';
 
-// import PrivateRoute from "./components/PrivateRoute";
 
-import withContext from "./components/Context";
+import PrivateRoute from "./components/PrivateRoute";
 
-//const AuthWithContext = withContext(Authenticated);
-//const UserSignInWithContext = withContext(UserSignIn);
+import withContext from "./components/Context/Context";
+
+const AuthWithContext = withContext(Authenticated);
+const UserSignInWithContext = withContext(UserSignIn);
 const UserSignUpWithContext = withContext(UserSignUp);
 const HeaderWithContext = withContext(Header);
-//const UserSignOutWithContext = withContext(UserSignOut);
+const UserSignOutWithContext = withContext(UserSignOut);
+const CourseDetailWithContext = withContext(CourseDetail)
+const CoursesWithContext = withContext(Courses);
+const CreateCourseWithContext = withContext(CreateCourse);
+const UpdateCourseWithContext = withContext(UpdateCourse);
 
-// import Courses from "./components/Courses/Courses";
 
 //use as main container
-function App() {
+export default () => {
   return (
     <Router>
       <div>
         <HeaderWithContext />
         <Switch>
-          <Route exact path="/" element={Public} />
-          {/* <PrivateRoute path="/authenticated" element={AuthWithContext} />
-          <Route path="/signin" element={UserSignInWithContext} /> */}
-          <Route path="/signup" element={UserSignUpWithContext} />
-          {/* <Route path="/signout" element={UserSignOutWithContext} />
-          <Route element={NotFound} /> */}
+          <Route path='/courses/:id/update' component= {UpdateCourseWithContext} />
+          <PrivateRoute path='/courses/create' component={CreateCourseWithContext}/>
+          <Route exact path="/" component={CoursesWithContext} />
+          <Route path='/courses/:id' component={CourseDetailWithContext} />
+          <PrivateRoute path="/authenticated" component={AuthWithContext} />
+          <Route path="/signin" component={UserSignInWithContext} />
+          <Route path="/signup" component={UserSignUpWithContext} />
+          <Route path="/signout" component={UserSignOutWithContext} />
+          <Route component={NotFound} />
         </Switch>
       </div>{" "}
     </Router>
   );
 }
-
-export default App;
