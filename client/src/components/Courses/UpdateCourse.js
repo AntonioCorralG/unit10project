f3dfea6e08c9dbdeea2ae97716c1ef899baff037
AntonioCorralG <97654161+AntonioCorralG@users.noreply.github.com>
2022-08-21 13:38:42 -0700

    code cleansing

--- client/src/components/Courses/UpdateCourse.js
+++ client/src/components/Courses/UpdateCourse.js
@@ -1,4 +1,4 @@
-// This component will allow a user to update the course if they are the owner 
+// This component will allow a user to update the course if they are the owner
 import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../Context/Context";
 import { Link, useHistory, useParams } from "react-router-dom";
@@ -14,17 +14,18 @@ const UpdateCourse = () => {
     data
       .fetchCourseDetailsWithID(id)
       .then((course) => {
-        setCourse(course)})
+        setCourse(course);
+      })
       .catch((err) => console.log(err));
   }, [data, id]);
 
-// Handles the user input ans updates the course details on the page
+  // Handles the user input ans updates the course details on the page
   const changeHandler = (event) => {
     const { name, value } = event.target;
     setCourse((course) => ({ ...course, [name]: value }));
   };
 
-// Submit the user changes for course update
+  // Submit the user changes for course update
   const submitHandler = (event) => {
     event.preventDefault();
     data
@@ -41,79 +42,84 @@ const UpdateCourse = () => {
       });
   };
   return (
-    
     <div className="wrap">
       {loggedInuser?.id === course?.userId ? (
-        <div><h2> Update Course</h2>
-        {errors.length ? (
-          <div>
-            <div className="validation--errors">
-              <h3> Validation Errors! </h3>
-              <ul>
-                {errors.map((error, index) => (
-                  <li key={index}>{error}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        ) : (
-          <div></div>
-        )}
-        <form onSubmit={submitHandler}>
-          <div classname="main--flex">
+        <div>
+          <h2> Update Course</h2>
+          {errors.length ? (
             <div>
-              <label htmlFor="title"> Course Title </label>
-              <input
-                onChange={changeHandler}
-                id="title"
-                name="title"
-                type="text"
-                value={course?.title}
-              />
-              <p>
-                By: {loggedInuser?.firstName} {loggedInuser?.lastName}
-              </p>
-  
-              <label htmlFor="description">Course Description </label>
-              <input
-                onChange={changeHandler}
-                id="description"
-                name="description"
-                value={course?.description}
-              />
+              <div className="validation--errors">
+                <h3> Validation Errors! </h3>
+                <ul>
+                  {errors.map((error, index) => (
+                    <li key={index}>{error}</li>
+                  ))}
+                </ul>
+              </div>
             </div>
-          </div>
-          <div>
-            <label htmlFor="estimatedTime"> Estimated Time: </label>
-            <input
-              onChange={changeHandler}
-              id="estimatedTime"
-              name="estimatedTime"
-              type="text"
-              value={course?.materialsNeeded}
-            />
-            <label htmlFor="materialsNeeded"> Materials Needed:</label>
-            <input
-              onChange={changeHandler}
-              id="materialsNeeded"
-              name="materialsNeeded"
-              value={course?.materialsNeeded}
-            />{" "}
-          </div>
-          <button className="button" type='submit'>
-            Update Course
-          </button>
-          <Link className="button button-secondary" to={`/courses/${id}`}>
-            Cancel
-          </Link>
-        </form></div>
-      ) : (
-        <div> <p>You are not authorised to update the course.</p>
-          <div className="wrap">
-          <Link to="/" className="button button-secondary">
-          List courses
-        </Link>
+          ) : (
+            <div></div>
+          )}
+          <form onSubmit={submitHandler}>
+            <div className="main--flex">
+              <div>
+                <div>
+                  <label htmlFor="title"> Course Title </label>
+                  <input
+                    onChange={changeHandler}
+                    id="title"
+                    name="title"
+                    type="text"
+                    value={course?.title}
+                  />
+                  <p>
+                    By: {loggedInuser?.firstName} {loggedInuser?.lastName}
+                  </p>
+
+                  <label htmlFor="description">Course Description </label>
+                  <textarea
+                    onChange={changeHandler}
+                    id="description"
+                    name="description"
+                    value={course?.description}
+                  />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="estimatedTime"> Estimated Time: </label>
+                <input
+                  onChange={changeHandler}
+                  id="estimatedTime"
+                  name="estimatedTime"
+                  type="text"
+                  value={course?.materialsNeeded}
+                />
+                <label htmlFor="materialsNeeded"> Materials Needed:</label>
+                <textarea
+                  onChange={changeHandler}
+                  id="materialsNeeded"
+                  name="materialsNeeded"
+                  value={course?.materialsNeeded}
+                />{" "}
+              </div>
+            </div>
+            <button className="button" type="submit">
+              Update Course
+            </button>
+            <Link className="button button-secondary" to={`/courses/${id}`}>
+              Cancel
+            </Link>
+          </form>
         </div>
+      ) : (
+        <div>
+          {" "}
+          <p>You are not authorised to update the course.</p>
+          <div className="wrap">
+            <Link to="/" className="button button-secondary">
+              List courses
+            </Link>
+          </div>
         </div>
       )}
     </div>
